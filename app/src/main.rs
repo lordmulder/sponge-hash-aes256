bc79d71036cb9753af6706a485c8ee74a7de941e
LoRd_MuldeR <mulder2@gmx.de>
2025-10-01 00:54:21 +0200

    Added initial version of the command-line frontend.

---
+++ app/src/main.rs
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: 0BSD
+// Copyright (C) 2025 by LoRd_MuldeR <mulder2@gmx.de>
+
+use sponge_hash_aes256::version;
+
+fn main() {
+    println!("SpongeHash-AES256 Tool v{}", version());
+}
