5a0a3613544cf5dc2226be1055d7d6867f8598a8
LoRd_MuldeR <mulder2@gmx.de>
2025-10-05 22:10:01 +0200

    Added build script for FreBSD to the Github workflow (CI).

---
+++ .build/netbsd/crossbuild-linux.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+set -e
+cd -- "$(dirname -- "${BASH_SOURCE[0]}")"
+
+if [[ ! -f "/opt/netbsd/sysroot/amd64/usr/lib/librt.so.1" || ! -f "/opt/netbsd/sysroot/amd64/usr/lib/crt0.o" ]]; then
+    echo "Error: NetBSD 'amd64' sysroot not found!"
+    exit 1
+fi
+
+export CARGO_TARGET_X86_64_UNKNOWN_NETBSD_LINKER=clang
+export CARGO_TARGET_X86_64_UNKNOWN_NETBSD_RUSTFLAGS="-Dwarnings -Ctarget-feature=+crt-static -Clink-arg=-s -Clink-arg=-fuse-ld=lld -Clink-arg=--target=x86_64-unknown-netbsd -Clink-arg=--sysroot=/opt/netbsd/sysroot/amd64 -Clink-arg=-L/opt/netbsd/sysroot/amd64/usr/lib"
+
+make
