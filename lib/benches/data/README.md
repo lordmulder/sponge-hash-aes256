4519664c32d6e7d0281e5e381bee81a542e027ff
LoRd_MuldeR <mulder2@gmx.de>
2025-10-12 15:19:04 +0200

    Some improvements to the benchmarking (stress test) application.

--- lib/benches/data/README.md
+++ lib/benches/data/README.md
@@ -2,28 +2,21 @@
 
 Please put the input file for stress testing called **`input.txt`** into this directory!
 
-## Test File #1
+Alternatively, the input file may be specified via the environment variable:  
+`SPONGE_BENCH_INPUT_FILE`
 
-A plain text file containing 32 million random and unique passwords ranging from 1 to 64 characters in length can be found here:
+## Processing
 
-* <https://sourceforge.net/projects/muldersoft/files/Test-Data/32M-random-unique-passwords.zip/download>
+Every line in the input file will be hashed as a separate input string.
 
-* <https://mega.nz/file/39YEXQyB#8eDW9QLo3zwBIegsEH_D-Ba8jhKz2EmndkT-p-JWZZ8>
+All generated hash values (digests) are added to a hash table to check them for uniqueness.
 
-* <https://www.mediafire.com/file/0avie1gwpadqu39/32M-random-unique-passwords.zip/file>
+If any collisions, i.e., duplicate hash values, are found, the test fails!
 
-* <https://drive.google.com/file/d/1I8G8Vd9ZRFhHuTyR_rnGeWwOmIffJs50/view?usp=sharing>
+## Test Files
 
-* <https://www.dropbox.com/scl/fi/0zwcgui4tytvmupdk1uv5/32M-random-unique-passwords.zip?rlkey=gt1zv34z2evag14peendqsgzr&st=41p37uji&dl=0>
+Test files containing 512 million random and unique passwords are available here:
 
-## Test File #2
+* <https://github.com/lordmulder/hash-function-test-files>
 
-A plain text file containing 64 million random and unique passwords ranging from 1 to 64 characters in length can be found here:
-
-* <https://sourceforge.net/projects/muldersoft/files/Test-Data/64M-random-unique-passwords.zip/download>
-
-* <https://mega.nz/file/ac1DBIQB#OqUcAHA0BeVOfBsBG_5JlEuBnL2d1RyGMZmzOiBokVY>
-
-* <https://www.mediafire.com/file/5zipky4kbb6knyp/64M-random-unique-passwords.zip/file>
-
-* <https://drive.google.com/file/d/1vOMG3qCW_dX4DlnJpYZADTK9sBSgNYNB/view?usp=drive_link>
+* <https://codeberg.org/MuldeR/hash-function-test-files>
--- lib/benches/stress_test.rs
+++ lib/benches/stress_test.rs
@@ -5,6 +5,7 @@
 use hex::encode_to_slice;
 use sponge_hash_aes256::{DEFAULT_DIGEST_SIZE, compute as compute_hash};
 use std::{
+    borrow::Cow,
     collections::HashSet,
     fs::File,
     io::{BufRead, BufReader},
@@ -17,18 +18,21 @@ use std::{
 // Stress test
 // ---------------------------------------------------------------------------
 
-const UPDATE_CYCLE: u64 = 997u64;
+const PROGRESS_UPDATE_CYCLE: usize = 9973usize;
 
 fn stress_test() {
-    let mut digest: [u8; DEFAULT_DIGEST_SIZE];
-    let mut hex_buffer = [0u8; 64usize];
     let mut set: HashSet<[u8; DEFAULT_DIGEST_SIZE]> = HashSet::new();
-    let mut counter = 0u64;
+    let mut counter = 1usize;
 
-    let file = match File::open(Path::new("benches").join("data").join("input.txt")) {
+    let input_file_name: Cow<Path> = match option_env!("SPONGE_BENCH_INPUT_FILE").map(str::trim_ascii) {
+        Some(path) if !path.is_empty() => Path::new(path).into(),
+        _ => Path::new(env!("CARGO_MANIFEST_DIR")).join("benches").join("data").join("input.txt").into(),
+    };
+
+    let file = match File::open(&input_file_name) {
         Ok(file_handle) => file_handle,
         Err(error) => {
-            eprintln!("Failed to open input file 'input.txt' for reading: {:?}", error);
+            eprintln!("Failed to open input file {input_file_name:?} for reading: {:?}", error);
             process::exit(1);
         }
     };
@@ -38,20 +42,7 @@ fn stress_test() {
             Ok(content) => {
                 let item = content.trim();
                 if !item.is_empty() {
-                    digest = compute_hash(None, item.as_bytes());
-                    counter += 1u64;
-                    let success = set.insert(digest);
-                    if (!success) || (counter % UPDATE_CYCLE == 0u64) {
-                        if encode_to_slice(&digest, &mut hex_buffer).is_ok() {
-                            let digest_string = unsafe { str::from_utf8_unchecked(&hex_buffer) };
-                            println!("[{:0>8}] {} << {:?}", counter, digest_string, item);
-                        }
-                    }
-                    if !success {
-                        eprintln!("Colission has been detected!");
-                        eprintln!("The value that caused the colission is: {:?}", item);
-                        process::exit(1);
-                    }
+                    process_input(&mut set, &mut counter, item);
                 }
             }
             Err(error) => {
@@ -65,11 +56,32 @@ fn stress_test() {
     println!("Total number of unique items is: {}", set.len());
 }
 
+#[inline(always)]
+fn process_input(set: &mut HashSet<[u8; DEFAULT_DIGEST_SIZE]>, counter: &mut usize, input: &str) {
+    let digest = compute_hash(None, input.as_bytes());
+    let success = set.insert(digest);
+    if (!success) || (*counter >= PROGRESS_UPDATE_CYCLE) {
+        let mut hex_buffer = [0u8; 64usize];
+        if encode_to_slice(&digest, &mut hex_buffer).is_ok() {
+            let digest_string = unsafe { str::from_utf8_unchecked(&hex_buffer) };
+            println!("[{:0>9}] {} << {:?}", set.len(), digest_string, input);
+        }
+        *counter = 0usize;
+    }
+    if !success {
+        eprintln!("Collision has been detected!");
+        eprintln!("The value that caused the collision is: {:?}", input);
+        process::exit(1);
+    } else {
+        *counter += 1usize;
+    }
+}
+
 // ---------------------------------------------------------------------------
 // Main
 // ---------------------------------------------------------------------------
 
-const NUM_RUNS: usize = 5usize;
+const NUM_RUNS: usize = 3usize;
 
 fn main() {
     let mut measurements = [Duration::default(); NUM_RUNS];
