6149f68f91d54ee180e9fd65ff9aafd3d441d25f
LoRd_MuldeR <mulder2@gmx.de>
2025-12-12 02:19:09 +0100

    Updated Rust to version 1.92.0 in all GitHub workflows.

--- .build/linux/mk-docker.sh
+++ .build/linux/mk-docker.sh
@@ -3,6 +3,6 @@ set -eo pipefail
 cd -- "$(dirname -- "${BASH_SOURCE[0]}")"
 
 readonly IMAGE_NAME=lordmulder/rust-xbuild
-readonly IMAGE_VERS=1.91-trixie-r4
+readonly IMAGE_VERS=1.92-trixie-r2
 
 exec docker run --rm -v ../..:/workspace -w /workspace "${IMAGE_NAME}":"${IMAGE_VERS}" make -C .build/linux
