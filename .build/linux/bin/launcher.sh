12747d74faf240b9ecca9b721074180109eae052
LoRd_MuldeR <mulder2@gmx.de>
2026-01-25 15:04:32 +0100

    Small improvement to the Linux launcher script.

--- .build/linux/bin/launcher.sh
+++ .build/linux/bin/launcher.sh
@@ -1,30 +1,25 @@
-#!/bin/bash
+#!/bin/sh
 set -eu
-readonly INSTALL_DIR="{{SPONGE256SUM_INSTDIR}}"
+INSTALL_DIR="{{SPONGE256SUM_INSTDIR}}"
+LSCPU_FLAGS="$(lscpu 2>/dev/null | grep -E -m1 '^Flags:' | cut -d':' -f2- | sed 's/^[[:space:]]*//;s/[[:space:]]*$//')"
 
-CPU_FEATURES="_undefined_"
 cpu_features() {
-    if [ "${CPU_FEATURES}" = "_undefined_" ]; then
-        CPU_FEATURES="$(lscpu 2>/dev/null | grep -E '^Flags:' | sed -E 's/^Flags:\s*//;s/(\S+)\s*/[\1]/g')"
-    fi
     for flag in "$@"; do
-        if [[ "${CPU_FEATURES}" != *"[${flag}]"* ]]; then
+        if ! printf '%s\n' "${LSCPU_FLAGS}" | grep -qw "${flag}"; then
             return 1
         fi
     done
 }
 
-if [[ ! -v SPONGE256SUM_ARCH || -z "${SPONGE256SUM_ARCH}" ]]; then
+if [ -z "${SPONGE256SUM_ARCH:=}" ]; then
     case "$(uname -m)" in
         x86_64 | amd64)
             SPONGE256SUM_ARCH="x86_64"
-            if cpu_features cx16 lahf_lm popcnt sse4_1 sse4_2 ssse3; then
-                if cpu_features avx avx2 bmi1 bmi2 f16c fma abm movbe xsave; then
+            if cpu_features cx16 lahf_lm popcnt abm ssse3 sse4_1 sse4_2 f16c fma avx avx2 xsave bmi1 bmi2 movbe; then
                     SPONGE256SUM_ARCH="x86_64-v3"
-                fi
             fi
             if cpu_features aes; then
-                SPONGE256SUM_ARCH="${SPONGE256SUM_ARCH}-aes_ni"
+                SPONGE256SUM_ARCH="${SPONGE256SUM_ARCH}-aes"
             fi
             ;;
         i?86 | x86 | x86pc)
@@ -32,7 +27,7 @@ if [[ ! -v SPONGE256SUM_ARCH || -z "${SPONGE256SUM_ARCH}" ]]; then
             if cpu_features cmov fxsr mmx sse sse2; then
                 SPONGE256SUM_ARCH="i686-sse2"
                 if cpu_features aes; then
-                    SPONGE256SUM_ARCH="${SPONGE256SUM_ARCH}-aes_ni"
+                    SPONGE256SUM_ARCH="${SPONGE256SUM_ARCH}-aes"
                 fi
             fi
             ;;
@@ -46,7 +41,7 @@ if [[ ! -v SPONGE256SUM_ARCH || -z "${SPONGE256SUM_ARCH}" ]]; then
             SPONGE256SUM_ARCH="riscv64gc"
             ;;
         *)
-            echo "Unknown architecture: $(uname -m)" >&2
+            echo "Unknown architecture encountered: $(uname -m)" >&2
             exit 1
             ;;
     esac
--- .build/windows/make.cmd
+++ .build/windows/make.cmd
@@ -127,9 +127,9 @@ for %%t in (x86_64 i686) do (
 	cargo clean || goto:error
 	cargo build --release --target %%t-pc-windows-msvc --features with-mimalloc --verbose || goto:error
 	if "%%t" == "i686" (
-		copy /B /Y "%CARGO_TARGET_DIR%\%%t-pc-windows-msvc\release\sponge256sum.exe" "out\target\release\sponge256sum-i686-sse2-aes_ni.exe" || goto:error
+		copy /B /Y "%CARGO_TARGET_DIR%\%%t-pc-windows-msvc\release\sponge256sum.exe" "out\target\release\sponge256sum-i686-sse2-aes.exe" || goto:error
 	) else (
-		copy /B /Y "%CARGO_TARGET_DIR%\%%t-pc-windows-msvc\release\sponge256sum.exe" "out\target\release\sponge256sum-%%t-aes_ni.exe" || goto:error
+		copy /B /Y "%CARGO_TARGET_DIR%\%%t-pc-windows-msvc\release\sponge256sum.exe" "out\target\release\sponge256sum-%%t-aes.exe" || goto:error
 	)
 )
 
@@ -137,7 +137,7 @@ for %%v in (v2 v3) do (
 	set "RUSTFLAGS=%DEFAULT_RUSTFLAGS% -Ctarget-cpu=x86-64-%%v -Ctarget-feature=+aes"
 	cargo clean || goto:error
 	cargo build --release --target x86_64-pc-windows-msvc --features with-mimalloc --verbose || goto:error
-	copy /B /Y "%CARGO_TARGET_DIR%\x86_64-pc-windows-msvc\release\sponge256sum.exe" "out\target\release\sponge256sum-x86_64-%%v-aes_ni.exe" || goto:error
+	copy /B /Y "%CARGO_TARGET_DIR%\x86_64-pc-windows-msvc\release\sponge256sum.exe" "out\target\release\sponge256sum-x86_64-%%v-aes.exe" || goto:error
 )
 
 REM ~~~~~~~~~~~~~~~~~~~~~~~~~~~~
@@ -164,9 +164,9 @@ for %%t in (x86_64 i686) do (
 	cargo clean || goto:error
 	cargo build -Zbuild-std=std,panic_abort --release --target %%t-win7-windows-msvc --verbose || goto:error
 	if "%%t" == "i686" (
-		copy /B /Y "%CARGO_TARGET_DIR%\%%t-win7-windows-msvc\release\sponge256sum.exe" "out\target\release\extra\sponge256sum-win7-i686-sse2-aes_ni.exe" || goto:error
+		copy /B /Y "%CARGO_TARGET_DIR%\%%t-win7-windows-msvc\release\sponge256sum.exe" "out\target\release\extra\sponge256sum-win7-i686-sse2-aes.exe" || goto:error
 	) else (
-		copy /B /Y "%CARGO_TARGET_DIR%\%%t-win7-windows-msvc\release\sponge256sum.exe" "out\target\release\extra\sponge256sum-win7-%%t-aes_ni.exe" || goto:error
+		copy /B /Y "%CARGO_TARGET_DIR%\%%t-win7-windows-msvc\release\sponge256sum.exe" "out\target\release\extra\sponge256sum-win7-%%t-aes.exe" || goto:error
 	)
 )
 
