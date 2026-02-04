4aa04761eed1895ef5969ca097acb95c6c4dfb80
LoRd_MuldeR <mulder2@gmx.de>
2026-02-04 23:06:32 +0100

    Github workflow (CI): Updated the 'rust-xbuild' Docker image to Rust version 1.93.0.

--- .build/linux/mk-docker.sh
+++ .build/linux/mk-docker.sh
@@ -5,6 +5,6 @@ cd -- "$(realpath -- "$(dirname -- "${BASH_SOURCE[0]}")")"
 unset RUSTC_BOOTSTRAP
 
 readonly IMAGE_NAME=lordmulder/rust-xbuild
-readonly IMAGE_VERS=1.93-trixie-r1
+readonly IMAGE_VERS=1.93-trixie-r2
 
 exec docker run --rm -v "${PWD}/../..":/workspace:ro -v "${PWD}/out":/workspace/.build/linux/out --tmpfs /tmp/rust-build:rw,exec -w /workspace "${IMAGE_NAME}":"${IMAGE_VERS}" make -C .build/linux
