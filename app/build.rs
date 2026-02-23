4e31a15443c8c68a391b85f17a089feab76f7447
LoRd_MuldeR <mulder2@gmx.de>
2026-02-23 22:17:37 +0100

    Small improvement to the generation of the Windows resources.

--- app/build.rs
+++ app/build.rs
@@ -8,10 +8,16 @@
 
 #[cfg(windows)]
 fn main() {
+    let target_arch = match std::env::var("CARGO_CFG_TARGET_ARCH") {
+        Ok(arch) if arch.eq("x86") => "i686".to_string(),
+        Ok(arch) => arch,
+        Err(_) => panic!("TARGET_ARCH is *not* specified!"),
+    };
+
     let mut winres = winres::WindowsResource::new();
     winres.set_icon(r"resources\app.ico");
-    winres.set_manifest_file(r"resources\app.manifest");
-    winres.compile().expect("Winres failed!");
+    winres.set_manifest_file(&format!(r"resources\app-{target_arch}.manifest"));
+    winres.compile().expect("Windows resource compiler has failed!");
 }
 
 #[cfg(not(windows))]
