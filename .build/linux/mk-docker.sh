a0fda9f1da8ddadd7a7965b53d39bc2f7f5d87a1
LoRd_MuldeR <mulder2@gmx.de>
2026-03-01 14:46:25 +0100

    Small improvement to the Linux build script ("build with Docker" script).

--- .build/linux/mk-docker.sh
+++ .build/linux/mk-docker.sh
@@ -4,7 +4,11 @@ cd -- "$(realpath -- "$(dirname -- "${BASH_SOURCE[0]}")")"
 
 unset RUSTC_BOOTSTRAP
 
-readonly IMAGE_NAME=lordmulder/rust-xbuild
-readonly IMAGE_VERS=1.93-trixie-r7
+readonly IMAGE_SPEC="$(cat ../../.github/workflows/ci.yml | grep -Pom1 'lordmulder/rust-xbuild:[^\s:]+')"
+if [ -z "${IMAGE_SPEC}" ]; then
+	echo "Error: Failed to determine Docker image version!"
+	exit 1
+fi
 
-exec docker run --rm -v "${PWD}/../..":/workspace:ro -v "${PWD}/out":/workspace/.build/linux/out --tmpfs /tmp/rust-build:rw,exec -w /workspace "${IMAGE_NAME}":"${IMAGE_VERS}" make -C .build/linux
+set -x
+exec docker run --rm -v "${PWD}/../..":/workspace:ro -v "${PWD}/out":/workspace/.build/linux/out --tmpfs /tmp/rust-build:rw,exec -w /workspace "${IMAGE_SPEC}" make -C .build/linux
