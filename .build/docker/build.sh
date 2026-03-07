6c65fe3bc6b00f1cef1727305079650088a9b7a0
LoRd_MuldeR <mulder2@gmx.de>
2026-03-07 17:38:21 +0100

    Github workflow (CI): Added new job to build and publish the Docker image.

--- .build/docker/build.sh
+++ .build/docker/build.sh
@@ -10,11 +10,11 @@ fi
 rm -rf bin/386 bin/amd64 bin/arm64 bin/LICENSE bin/BUILD_INFO
 mkdir -p bin/386 bin/amd64 bin/arm64
 
-cp -f ../linux/out/target/release/sponge256sum-i586 ../linux/out/target/release/sponge256sum-i686* bin/386/
-cp -f ../linux/out/target/release/sponge256sum-x86_64* bin/amd64/
-cp -f ../linux/out/target/release/sponge256sum-aarch64* bin/arm64/
-cp -f ../linux/out/target/release/LICENSE ../linux/out/target/release/BUILD_INFO bin/
-cp -f ../../app/tests/data/text/*.txt resources/data/
+cp -vf ../linux/out/target/release/LICENSE ../linux/out/target/release/BUILD_INFO bin/
+cp -vf ../linux/out/target/release/sponge256sum-i586 ../linux/out/target/release/sponge256sum-i686* bin/386/
+cp -vf ../linux/out/target/release/sponge256sum-x86_64* bin/amd64/
+cp -vf ../linux/out/target/release/sponge256sum-aarch64* bin/arm64/
+cp -vf ../../app/tests/data/text/*.txt resources/data/
 
 for docker_arch in 386 amd64 arm64; do
 	( set -x ; docker buildx build --platform=linux/${docker_arch} . )
