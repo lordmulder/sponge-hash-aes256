4f5947a345b2f8b901fb9ac564660c4bc50ef02c
LoRd_MuldeR <mulder2@gmx.de>
2026-03-22 20:36:59 +0100

    Github workflow (CI): Updated the 'rust-xbuild' Docker image to Rust version 1.94.0 (R2).

--- .build/linux/mk-docker.sh
+++ .build/linux/mk-docker.sh
@@ -4,7 +4,7 @@ cd -- "$(realpath -- "$(dirname -- "${BASH_SOURCE[0]}")")"
 
 unset RUSTC_BOOTSTRAP
 
-readonly IMAGE_SPEC="$(cat ../../.github/workflows/ci.yml | grep -Pom1 'lordmulder/rust-xbuild:[^\s:]+')"
+readonly IMAGE_SPEC="$(cat ../../.github/workflows/ci.yml | grep -PA4 '^\s*build-linux:' | grep -Pom1 'lordmulder/rust-xbuild:[^\s:]+')"
 if [ -z "${IMAGE_SPEC}" ]; then
 	echo "Error: Failed to determine Docker image version!"
 	exit 1
@@ -12,4 +12,3 @@ fi
 
 set -x
 exec docker run --rm -v "${PWD}/../..":/workspace:ro -v "${PWD}/out":/workspace/.build/linux/out --tmpfs /tmp/rust-build:rw,exec -w /workspace "${IMAGE_SPEC}" make -C .build/linux
-
